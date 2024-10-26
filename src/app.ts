2016a4549c4af5eb15d379c6600b7fa281d9ddde
MD HABIBUR RAHMAN <mdhabibur.hr7@gmail.com>
2024-10-26 11:06:24 +0600

    basic project setup completed

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import cors from "cors";
+import express, { Application, Request, Response } from "express";
+const app: Application = express();
+// const port = 3000;
+
+//parser
+app.use(express.json());
+app.use(cors());
+app.get("/", (req: Request, res: Response) => {
+  // const a = 10;
+  res.send("Hello world");
+});
+
+export default app;
